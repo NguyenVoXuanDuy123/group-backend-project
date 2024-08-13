7e0ceba56ee03d7f0021a48c7c649c97dd9e5812
NguyenVoXuanDuy123 <147340503+NguyenVoXuanDuy@users.noreply.github.com>
2024-08-14 05:57:16 +0700

    Update post.controller.ts

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -2,7 +2,7 @@ import { APIRequest, APIResponse } from "@src/types/api.types";
 
 class PostController {
   public async createPost(req: APIRequest, res: APIResponse) {
-    return res.status(200).json({ message: "Post created successfully" });
+    // sẽ làm ở đây sau
   }
 }
 
