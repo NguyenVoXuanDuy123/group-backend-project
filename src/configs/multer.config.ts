e6e2f368bbdb4c594f467bb77b4ffb54f59ae5f6
NguyenVoXuanDuy123 <147340503+NguyenVoXuanDuy@users.noreply.github.com>
2024-09-08 16:50:06 +0700

    fix bug

--- src/configs/multer.config.ts
+++ src/configs/multer.config.ts
@@ -1,9 +1,5 @@
-import multer, { FileFilterCallback } from "multer";
-import { Request } from "express";
-import path from "path";
+import multer from "multer";
 import { UPLOAD_DIR } from "@src/constant/dir";
-import ApiError from "@src/error/ApiError";
-import ApiErrorCodes from "@src/error/ApiErrorCodes";
 
 // Multer configuration
 const storage = multer.diskStorage({
@@ -15,25 +11,8 @@ const storage = multer.diskStorage({
   },
 });
 
-const fileFilter = (
-  req: Request,
-  file: Express.Multer.File,
-  cb: FileFilterCallback
-) => {
-  const fileTypes = /jpeg|jpg|png/;
-  const extname = fileTypes.test(path.extname(file.originalname).toLowerCase());
-  const mimetype = fileTypes.test(file.mimetype);
-
-  if (extname && mimetype) {
-    return cb(null, true);
-  } else {
-    throw new ApiError(ApiErrorCodes.INVALID_IMAGE_FORMAT);
-  }
-};
-
 const upload = multer({
   storage,
-  fileFilter,
   limits: { fileSize: 5 * 1024 * 1024 },
 });
 
